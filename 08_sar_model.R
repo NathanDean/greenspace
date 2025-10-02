6bf4db7477245e0e99fd4c87fc58df2f19eb9ead
Nathan Dean <nathandean@hotmail.co.uk>
2025-10-02 23:53:48 +0100

    Add nested cross-validation to SAR model

--- 08_sar_model.R
+++ 08_sar_model.R
@@ -5,47 +5,73 @@ set.seed(42)
 library(spatialreg)
 library(spdep)
 library(sf)
+library(sfdep)
 library(Metrics)
 
 # Load data
-df <- read.csv("datasets/5_split/df_fe.csv")
-
-# Extract fold ids
-fold_ids <- df$fold_id_r
-folds <- unique(fold_ids)
-
-# Drop unneeded cols
-drop_cols <- c("fold_id_python", "fold_id_r")
+df <- st_read("datasets/5_split/df_fe.gpkg")
+drop_cols <- names(df)[grepl("python", names(df))]
 df <- df[, !names(df) %in% drop_cols]
 
-# Define spatial weight calculation function
-create_spatial_weights <- function(df) {
+# Extract fold ids
+outer_fold_ids <- df$outer_loop_fold_id_r
+outer_splits <- sort(unique(outer_fold_ids))
+inner_fold_ids <- df[, grepl("inner_loop", names(df)), drop = FALSE]
+inner_splits <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
-  coords <- cbind(df$x_coord, df$y_coord)   # Create coordinate feature
-  knn <- knearneigh(coords, k = 4)    # Calculate k nearest neighbours using coords
-  w <- nb2listw(knn2nb(knn), zero.policy = TRUE)    # Calculate weights
+# Drop unneeded cols
+drop_cols <- names(df)[grepl("fold_id", names(df))]
+df <- df[, !names(df) %in% drop_cols]
+
+# Function - Create spatial weight matrix
+create_spatial_weights <- function(df, hps) {
+
+  weighting_method <- hps$weighting_method
+  coords <- st_centroid(st_geometry(df))
+  w <- NULL
+
+  if (weighting_method == "distance") {
+    print(weighting_method)
+    print(hps$max_distance)
+    dnn <- dnearneigh(coords, d1 = 200, d2 = hps$max_distance)
+    w <- nb2listw(dnn, zero.policy = TRUE)
+  } else if (weighting_method == "knn") {
+    print(weighting_method)
+    print(hps$k)
+    knn <- knearneigh(coords, k = hps$k)
+    w <- nb2listw(knn2nb(knn), zero.policy = TRUE)
+  } else {
+    print(weighting_method)
+    print(hps$is_queen)
+    nb <- st_contiguity(df, hps$is_queen)
+    w <- nb2listw(nb, zero.policy = TRUE)
+  }
 
   # Return weights
   w
 
 }
 
-# Define build model function
-build_model <- function(df) {
+# Function - Build model
+build_model <- function(df, hps) {
 
   # Create spatial weight matrix for train_df
   print("Creating spatial weight matrix for train_df...")
-  w <- create_spatial_weights(df)
+  w <- create_spatial_weights(df, hps)
+
+  df_no_geom <- st_drop_geometry(df)
+  rownames(df_no_geom) <- NULL
 
   # Fit SAR model using spatial weights
   print("Fitting model...")
   system.time(
     sar_model <- lagsarlm(
-      very_good_health ~ . - x_coord - y_coord,   # Predict very_good_health as a function of all other features apart from coords
-      data = df,
+      very_good_health ~ .,   # Predict very_good_health as a function of all other features apart from coords
+      data = df_no_geom,
       listw = w,    # Spatial weights to calculate lagged dependent variable
       method = "LU",    # Use LU decomposition to calculate lagged dependent variable
-      quiet = FALSE,
+      quiet = TRUE,
+      zero.policy = TRUE
     )
   )
 
@@ -54,53 +80,199 @@ build_model <- function(df) {
   
 }
 
-# Cross-validation loop
-for (fold in folds) {
+# Function - Get evaluation metrics for set of predictions
+get_evaluation_metrics <- function(labels, predictions) {
 
-  message <- paste("--- Training on fold ", fold, "---")
-  print(paste(message))
-
-  # Separate data into training and validation sets
-  is_in_validation_set <- fold_ids == fold
-  is_in_training_set <- !is_in_validation_set
-  train_df <- df[is_in_training_set, ]
-  validation_df <- df[is_in_validation_set, ]
-  rownames(validation_df) <- NULL   # Reset row numbers so they align with spatial weight matrix for predictions
-
-  # Build model using train_df
-  model <- build_model(train_df)
-  
-  # Create spatial weight matrix for validation_df
-  print("Creating spatial weight matrix for validation_df...")
-  validation_w <- create_spatial_weights(validation_df)
-
-  # Calculate predictions on validation_df
-  print("Calculating predictions...")
-  predictions <- predict(
-    model,
-    newdata = validation_df,
-    listw = validation_w,
-    zero.policy = TRUE
-  )  
-
-  # Calculate accuracy metrics
-  labels <- validation_df$very_good_health
   mae <- mae(labels, predictions)
   mse <- mse(labels, predictions)
   sum_of_squares_residual <- sum((labels - predictions) ^ 2)
   sum_of_squares_total <- sum((labels - mean(labels)) ^ 2)
   r2 <- 1 - (sum_of_squares_residual / sum_of_squares_total)
+  list(mae = mae, mse = mse, r2 = r2)
 
-  # Print metrics
-  results_message <- paste("--- Results for fold ", fold, "---")
-  print(results_message)
-  print(paste("MAE:", round(mae, 4)))
-  print(paste("MSE:", round(mse, 4)))
-  print(paste("R²:", round(r2, 4)))
-  print("")
+}
+
+# Function - Average prediction scores for set of cross-validation results
+get_avg_scores <- function(cv_results) {
+  mae_scores <- c()
+  mse_scores <- c()
+  r2_scores <- c()
+
+  for (result in cv_results) {
+    mae_scores <- c(mae_scores, result$mae)
+    mse_scores <- c(mse_scores, result$mse)
+    r2_scores <- c(r2_scores, result$r2)
+  }
+
+  mae <- mean(mae_scores)
+  mse <- mean(mse_scores)
+  r2 <- mean(r2_scores)
+
+  avgs <- list(mae = mae, mse = mse, r2 = r2)
+  avgs
+}
+
+# Function - Find hyperparameter combination that produced best results from a set of cross-validation results
+get_optimal_hps <- function(hp_combinations, cv_results) {
+  hp_combination_scores <- c()
+
+  for (hp_combination in seq_along(hp_combinations)) {
+    current_hp_combination_results <- cv_results[sapply(cv_results, function(result) result$hp_combination == hp_combination)]
+    avg_scores <- get_avg_scores(current_hp_combination_results)
+    mae <- avg_scores$mae
+    mse <- avg_scores$mse
+    r2 <- avg_scores$r2
+    hp_combination_scores <- c(hp_combination_scores, mse)
+  }
+
+  optimal_combination <- which.min(hp_combination_scores)
+  optimal_hps <- hp_combinations[[optimal_combination]]
+  optimal_hps
+
+}
+
+# Nested cross-validation loop
+outer_cv_results <- vector(mode = "list", length = 0)
+print(paste("outer_cv_results initialized:", exists("outer_cv_results")))
+
+for (current_outer_split in outer_splits) {
+
+  hp_combinations <- vector(mode = "list", length = 0)
+  inner_cv_results <- vector(mode = "list", length = 0)
+
+  # Separate data into training and validation sets
+  is_in_validation_set <- outer_fold_ids == current_outer_split
+  is_in_training_set <- !is_in_validation_set
+  outer_train_df <- df[is_in_training_set, ]
+  outer_validation_df <- df[is_in_validation_set, ]
+  current_inner_fold_ids <- inner_fold_ids[is_in_training_set, , drop = FALSE]    # Removes rows not in current training set
+  rownames(outer_train_df) <- NULL
+  rownames(outer_validation_df) <- NULL   # Reset row numbers so they align with spatial weight matrix for predictions
+  rownames(current_inner_fold_ids) <- NULL
+
+  # Loop to test 10 hyperparameter combinations
+  for (i in 1:10) {
+
+    # Get random hyperparameters
+    hps <- list()
+    # weighting_method <- sample(c("distance", "knn", "queen", "rook"), 1)
+    weighting_method <- "queen"
+    hps$weighting_method <- weighting_method
+    if (weighting_method == "distance") {
+      max_distance <- sample(1000:2000, 1)
+      hps$max_distance <- max_distance
+
+    } else if (weighting_method == "knn") {
+      k <- sample(2:30, 1)
+      hps$k <- k
+    } else {
+      is_queen <- sample(c(TRUE, FALSE), 1)
+      hps$is_queen <- is_queen
+    }
+    hp_combinations <- c(hp_combinations, list(hps))
+
+    # Inner cross-validation to evaluate hyperparameter combinations
+    for (current_inner_split in inner_splits) {
+
+      print(paste("--- Outer split ", current_outer_split, ": Training model ", i, " on inner split ", current_inner_split, "---"))
+
+      # Separate data into training and validation sets
+      is_in_validation_set <- current_inner_fold_ids[[paste0("inner_loop_", current_outer_split, "_fold_id_r")]] == current_inner_split
+      is_in_training_set <- !is_in_validation_set
+      inner_train_df <- outer_train_df[is_in_training_set, ]
+      inner_validation_df <- outer_train_df[is_in_validation_set, ]
+      rownames(inner_train_df) <- NULL
+      rownames(inner_validation_df) <- NULL   # Reset row numbers so they align with spatial weight matrix for predictions
+
+      # Build model using inner_train_df
+      print("Building model...")
+      model <- build_model(inner_train_df, hps)
+      
+      # Create spatial weight matrix for inner_validation_df
+      print("Creating spatial weight matrix...")
+      inner_validation_w <- create_spatial_weights(inner_validation_df, hps)
+
+      # Calculate predictions on inner_validation_df
+      print("Calculating predictions...")
+      inner_validation_df_no_geom <- st_drop_geometry(inner_validation_df)
+
+      predictions <- predict(
+        model,
+        newdata = inner_validation_df_no_geom,
+        listw = inner_validation_w,
+        zero.policy = TRUE
+      )  
+
+      # Calculate accuracy metrics
+      print("Evaluating predictions...")
+      labels <- inner_validation_df$very_good_health
+      metrics <- get_evaluation_metrics(labels, predictions)
+      mae <- metrics$mae
+      mse <- metrics$mse
+      r2 <- metrics$r2
+
+      current_split_results <- list(
+        hp_combination = i,
+        inner_split = current_inner_split,
+        hps = hps,
+        mae = mae,
+        mse = mse,
+        r2 = r2
+      )
+
+      inner_cv_results <- c(inner_cv_results, list(current_split_results))
+
+      # Remove large variables to avoid memory issues
+      rm(model, inner_validation_w, predictions)
+      gc()
+
+    }
+
+  }
+
+  print(paste("--- Outer split ", current_outer_split, ": Training on optimised model"))
+
+  optimal_hps <- get_optimal_hps(hp_combinations, inner_cv_results)
+
+  # Build model using train_df
+  print("Building model...")
+  model <- build_model(outer_train_df, optimal_hps)
+
+  # Create spatial weight matrix for validation_df
+  print("Creating spatial weight matrix...")
+  outer_validation_w <- create_spatial_weights(outer_validation_df, optimal_hps)
+
+  # Calculate predictions on validation_df
+  print("Calculating predictions...")
+  outer_validation_df_no_geom <- st_drop_geometry(outer_validation_df)
+  predictions <- predict(
+    model,
+    newdata = outer_validation_df_no_geom,
+    listw = outer_validation_w,
+    zero.policy = TRUE
+  )  
+
+  # Calculate accuracy metrics
+  print("Evaluating predictions...")
+  labels <- outer_validation_df$very_good_health
+  metrics <- get_evaluation_metrics(labels, predictions)
+  mae <- metrics$mae
+  mse <- metrics$mse
+  r2 <- metrics$r2
+
+  current_split_results <- list(
+    outer_split = current_outer_split,
+    hps = optimal_hps,
+    mae = mae,
+    mse = mse,
+    r2 = r2,
+    inner_cv_results = inner_cv_results
+  )
+
+  outer_cv_results <- c(outer_cv_results, list(current_split_results))
 
   # Remove large variables to avoid memory issues
-  rm(model, validation_w, predictions)
+  rm(model, outer_validation_w, predictions)
   gc()
 
 }
--- outputs/logs/sar_logs.md
+++ outputs/logs/sar_logs.md
@@ -6,9 +6,6 @@
 Error: cannot allocate vector of size 67.4 Mb
 In addition: There were 40 warnings (use warnings() to see them)
 
-> & C:/Users/Natha/Dev/irp/.venv/Scripts/Activate.ps1
-> Error: unexpected '&' in "&"
-> warnings()
 > Warning messages:
 > 1: In lagsarlm(very_good_health ~ ., data = df, listw = w) :
 > Aliased variables found: good_health fair_health bad_health very_bad_health f0 f1 f2 f3 f4 f5 f6 f7 f8 f9 f10 f11 f12 f13 f14 f15 f16 f17 f18 f19 f20 f21 f22 f23 f24 f25 f26 f27 f28 f29 f30 f31 f32 f33 f34 f35 f36 f37 f38 f39 f40 f41 f42 f43 f44 f45 f46 f47 f48 f49 f50 f51 f52 f53 f54 f55 f56 f57 f58 f59 f60 f61 f62 f63 f64 f65 f66 f67 f68 f69 f70 f71 f72 f73 f74 f75 f76 f77 f78 f79 f80 f81 f82 f83 f84 f85 f86 f87 f88 f89 f90 m0 m1 m2 m3 m4 m5 m6 m7 m8 m9 m10 m11 m12 m13 m14 m15 m16 m17 m18 m19 m20 m21 m22 m23 m24 m25 m26 m27 m28 m29 m30 m31 m32 m33 m34 m35 m36 m37 m38 m39 m40 m41 m42 m43 m44 m45 m46 m47 m48 m49 m50 m51 m52 m53 m54 m55 m56 m57 m58 m59 m60 m61 m62 m63 m64 m65 m66 m67 m68 m69 m70 m71 m72 m73 m74 m75 m76 m77 m78 m79 m80 m81 m82 m83 m84 m85 m86 m87 m88 m89 m90 white_british white_irish white_gypsy.irish_traveller white_roma white_other mixed_white_and_asian mixed_white_and_black_african mixed_white_and_black_caribbean mixed_other asian_bangladeshi asian_chinese asian_india [... truncated]
@@ -269,3 +266,11 @@ Hessian: rho: 0.5664625 function value: 7651.513
 Error in solve.default(-(mat), tol.solve = tol.solve) :
 system is computationally singular: reciprocal condition number = 1.12396e-20
 Timing stopped at: 137.4 0.44 138.5
+
+# With method = distance and small distance values
+
+neighbour object has subgraphs
+
+# With method = distance and distance ~2000
+
+neighbour object has 5 sub-graphs
