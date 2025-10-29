e4040b9731328cc9b0e593790f90ec06e21e52a2
Nathan Dean <nathandean@hotmail.co.uk>
2025-10-29 23:36:00 +0000

    Refactor ANN nested cross-validation code

--- utils/nested_cv/ann.py
+++ utils/nested_cv/ann.py
@@ -8,19 +8,32 @@ from utils.model_utils import (
 )
 
 
+def get_random_hyperparameters(features):
+    no_of_layers = np.random.randint(1, 5)
+    no_of_nodes = []
+    for i in range(0, no_of_layers):
+        no_of_nodes.append(
+            np.random.randint((len(features.columns) / 2), (len(features.columns) * 2))
+        )
+    batch_size = np.random.randint(10, 64)
+    learning_rate = np.random.uniform(0.0001, 0.1)
+    loss_function = np.random.choice(["mae", "mse"])
+    return no_of_layers, no_of_nodes, batch_size, learning_rate, loss_function
+
+
 def build_model(
-    train_features,
+    features,
     no_of_layers,
     no_of_nodes,
     learning_rate,
     loss_function,
-    regularised=False,
+    regularised,
 ):
 
     layers = []
 
     normaliser = keras.layers.Normalization(axis=-1)
-    normaliser.adapt(np.array(train_features))
+    normaliser.adapt(np.array(features))
     layers.append(normaliser)
 
     for layer_no in range(no_of_layers):
@@ -46,17 +59,47 @@ def build_early_stopper():
     return early_stopper
 
 
-def get_random_hyperparameters(features):
-    no_of_layers = np.random.randint(1, 5)
-    no_of_nodes = []
-    for i in range(0, no_of_layers):
-        no_of_nodes.append(
-            np.random.randint((len(features.columns) / 2), (len(features.columns) * 2))
-        )
-    batch_size = np.random.randint(10, 64)
-    learning_rate = np.random.uniform(0.0001, 0.1)
-    loss_function = np.random.choice(["mae", "mse"])
-    return no_of_layers, no_of_nodes, batch_size, learning_rate, loss_function
+def evaluate_model(
+    train_features,
+    train_labels,
+    val_features,
+    val_labels,
+    no_of_layers,
+    no_of_nodes,
+    learning_rate,
+    loss_function,
+    batch_size,
+    regularised,
+):
+    # Build model
+    model = build_model(
+        train_features,
+        no_of_layers,
+        no_of_nodes,
+        learning_rate,
+        loss_function,
+        regularised,
+    )
+    early_stopper = build_early_stopper()
+
+    # Fit model
+    model.fit(
+        train_features,
+        train_labels,
+        batch_size=batch_size,
+        epochs=200,
+        validation_split=0.2,
+        callbacks=[early_stopper],
+        verbose=0,
+    )
+
+    # Get predictions using fitted model
+    predictions = model.predict(val_features).flatten()
+
+    # Get accuracy scores
+    mae, mse, r2 = get_evaluation_metrics(val_labels, predictions)
+
+    return mae, mse, r2
 
 
 def evaluate_ann(df, regularised=False):
@@ -71,12 +114,13 @@ def evaluate_ann(df, regularised=False):
         features,
     ) = separate_features(df)
 
+    # Outer cross-validation loop to evaluate models
     for current_outer_split in outer_cv_splits:
 
         hp_combinations = []
-        cv_results = []
+        inner_cv_results = []
 
-        # Get training and validation sets for current outer split
+        # Get outer cross-validation splits
         (
             outer_train_features,
             outer_train_labels,
@@ -95,7 +139,7 @@ def evaluate_ann(df, regularised=False):
         # Loop to test 8 hyperparameter combinations
         for i in range(8):
 
-            # Get hyperparameters
+            # Get random hyperparameters
             no_of_layers, no_of_nodes, batch_size, learning_rate, loss_function = (
                 get_random_hyperparameters(features)
             )
@@ -109,13 +153,13 @@ def evaluate_ann(df, regularised=False):
             }
             hp_combinations.append(current_hps)
 
-            # Inner cross-validation for model selection
+            # Get inner cross-validation splits
             for current_inner_split in inner_cv_splits:
                 print(
                     f"\n --- Outer split {current_outer_split}: Training model {i} on inner split {current_inner_split} ---"
                 )
 
-                # Get training and validation sets for current inner split
+                # Separate features, labels and fold ids from df
                 (
                     inner_train_features,
                     inner_train_labels,
@@ -131,36 +175,22 @@ def evaluate_ann(df, regularised=False):
                     outer_train_labels,
                 )
 
-                # Build model
-                model = build_model(
+                # Evaluate model and return accuracy scores
+                mae, mse, r2 = evaluate_model(
                     inner_train_features,
+                    inner_train_labels,
+                    inner_val_features,
+                    inner_val_labels,
                     no_of_layers,
                     no_of_nodes,
                     learning_rate,
                     loss_function,
+                    batch_size,
                     regularised,
                 )
-                early_stopper = build_early_stopper()
 
-                # Fit model
-                model.fit(
-                    inner_train_features,
-                    inner_train_labels,
-                    batch_size=batch_size,
-                    epochs=200,
-                    validation_split=0.2,
-                    callbacks=[early_stopper],
-                    verbose=0,
-                )
-
-                # Get predictions using fitted model
-                predictions = model.predict(inner_val_features).flatten()
-
-                # Get accuracy scores
-                mae, mse, r2 = get_evaluation_metrics(inner_val_labels, predictions)
-
-                # Add scores for current fold to results
-                cv_results.append(
+                # Add scores to results
+                inner_cv_results.append(
                     {
                         "hp_combination": i,
                         "inner_split": current_inner_split,
@@ -173,43 +203,29 @@ def evaluate_ann(df, regularised=False):
 
         print(f"--- Outer split {current_outer_split}: Training on optimised model ---")
 
-        # Get optimal hyperparameters for current outer split training set
-        opt_hps = get_optimal_hyperparameters(hp_combinations, cv_results)
+        # Get optimal hyperparameters for current training set
+        opt_hps = get_optimal_hyperparameters(hp_combinations, inner_cv_results)
         opt_no_of_layers = opt_hps["no_of_layers"]
         opt_no_of_nodes = opt_hps["no_of_nodes"]
         opt_batch_size = opt_hps["batch_size"]
         opt_learning_rate = opt_hps["learning_rate"]
         opt_loss_function = opt_hps["loss_function"]
 
-        # Build model
-        model = build_model(
+        # Evaluate model and return accuracy scores
+        mae, mse, r2 = evaluate_model(
             outer_train_features,
+            outer_train_labels,
+            outer_val_features,
+            outer_val_labels,
             opt_no_of_layers,
             opt_no_of_nodes,
             opt_learning_rate,
             opt_loss_function,
+            opt_batch_size,
             regularised,
         )
-        early_stopper = build_early_stopper()
 
-        # Fit model
-        model.fit(
-            outer_train_features,
-            outer_train_labels,
-            batch_size=opt_batch_size,
-            epochs=200,
-            validation_split=0.2,
-            callbacks=[early_stopper],
-            verbose=0,
-        )
-
-        # Get predictions using fitted model
-        predictions = model.predict(outer_val_features).flatten()
-
-        # Get accuracy scores
-        mae, mse, r2 = get_evaluation_metrics(outer_val_labels, predictions)
-
-        # Add scores for current fold to results
+        # Add scores to results
         outer_cv_results.append(
             {
                 "outer_split": current_outer_split,
@@ -217,7 +233,7 @@ def evaluate_ann(df, regularised=False):
                 "mae": mae,
                 "mse": mse,
                 "r2": r2,
-                "inner_cv_results": cv_results,
+                "inner_cv_results": inner_cv_results,
             }
         )
 
